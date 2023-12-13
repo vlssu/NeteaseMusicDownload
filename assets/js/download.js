e4377ea6d063b3686375aa51da15fb7524fd7c85
飒爽师叔 <43847794+vlssu@users.noreply.github.com>
2023-12-13 16:26:19 +0800

    更新

--- assets/js/download.js
+++ assets/js/download.js
@@ -1,10 +1,26 @@
 function openNew(){
-    console.log('感谢使用GamerNoTitle提供的网易云音乐下载服务！')
+    console.log('感谢使用VLssu提供的网易云音乐下载服务！');
     var link = document.getElementById("link").value;
-    const NeteaseReg = new RegExp('music.163.com')
-    const schemeReg = new RegExp('^https?://')
-    if(NeteaseReg.test(link) && schemeReg.test(link)) {
-        var ToOpen = link.replace("music.163.com", "api.vlssu.com/meting").replace("/#/","/");  //防止解析暴毙
+    const NeteaseReg = new RegExp('music.163.com');
+    const schemeReg = new RegExp('^https?://');
+
+    if (NeteaseReg.test(link) && schemeReg.test(link)) {
+        // 提取id参数的值
+        var idMatch = link.match(/id=(\d+)/);
+        var idValue = idMatch ? idMatch[1] : '';
+
+        // 进行链接转换
+        var ToOpen = link
+            .replace("music.163.com", "api.vlssu.com/meting")
+            .replace("/#/","/")
+            .replace(/\?id=/, "?type=")
+            .replace(/&userid=\d+/, "")  // 删除&userid参数
+
+        // 如果成功提取到id参数的值，添加到新链接中
+        if (idValue) {
+            ToOpen = ToOpen + "&id=" + idValue;
+        }
+
         window.open(ToOpen);
     }
     else{
